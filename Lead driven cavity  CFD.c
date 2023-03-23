9304be197c13e36a612c57db98ad2f51c71084de
Vashu21 <113059396+Vashu21@users.noreply.github.com>
2023-03-24 01:32:16 +0530

    Update Lead driven cavity  CFD.c

--- Lead driven cavity  CFD.c
+++ Lead driven cavity  CFD.c
@@ -1,234 +1,160 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#define grid 128
-
-int main (void)
-{
-	double u[grid][grid+1], un[grid][grid+1], uc[grid][grid];
-	double v[grid+1][grid], vn[grid+1][grid], vc[grid][grid];
-	double p[grid+1][grid+1], pn[grid+1][grid+1], pc[grid][grid];
-	double m[grid+1][grid+1];
-	int i, j, step;
-	double dx, dy, dt, tau, delta, error, Re;
-	step =1;
-	dx = 1.0/(grid-1);
-	dy = 1.0/(grid-1);
-	dt = 0.001;
-	delta = 4.5;
-	error = 1.0;
-	Re = 400.0;
-	
-	// Initializing u
-		for (i=0; i<=(grid-1); i++)
-		{
-			for (j=0; j<=(grid); j++)
-			{
-				u[i][j] = 0.0;
-				u[i][grid] = 1.0;
-				u[i][grid-1] = 1.0;
-			}
-		}
-		
-	// Initializing v
-		for (i=0; i<=(grid); i++)
-		{
-			for (j=0; j<=(grid-1); j++)
-			{
-				v[i][j] = 0.0;
-			}
-		}
-		
-	// Initializing p
-		for (i=0; i<=(grid); i++)
-		{
-			for (j=0; j<=(grid); j++)
-			{
-				p[i][j] = 1.0;
-			}
-		}
-	
-	while (error > 0.00000001)
-	{
-		// Solve u-momentum equation
-		for (i=1; i<=(grid-2); i++)
-		{
-			for (j=1; j<=(grid-1); j++)
-			{
-				un[i][j] = u[i][j] - dt*(  (u[i+1][j]*u[i+1][j]-u[i-1][j]*u[i-1][j])/2.0/dx 
-							+0.25*( (u[i][j]+u[i][j+1])*(v[i][j]+v[i+1][j])-(u[i][j]+u[i][j-1])*(v[i+1][j-1]+v[i][j-1]) )/dy  )
-								- dt/dx*(p[i+1][j]-p[i][j]) 
-									+ dt*1.0/Re*( (u[i+1][j]-2.0*u[i][j]+u[i-1][j])/dx/dx +(u[i][j+1]-2.0*u[i][j]+u[i][j-1])/dy/dy );
-			}
-		}
-		
-		// Boundary conditions
-		for (j=1; j<=(grid-1); j++)
-		{
-			un[0][j] = 0.0;
-			un[grid-1][j] = 0.0;
-		}
-		
-		for (i=0; i<=(grid-1); i++)
-		{
-			un[i][0] = -un[i][1];
-			un[i][grid] = 2 - un[i][grid-1];
-		}
-		
-		
-		// Solves v-momentum
-		for (i=1; i<=(grid-1); i++)
-		{
-			for (j=1; j<=(grid-2); j++)
-			{
-				vn[i][j] = v[i][j] - dt* ( 0.25*( (u[i][j]+u[i][j+1])*(v[i][j]+v[i+1][j])-(u[i-1][j]+u[i-1][j+1])*(v[i][j]+v[i-1][j]) )/dx 
-							+(v[i][j+1]*v[i][j+1]-v[i][j-1]*v[i][j-1])/2.0/dy ) 
-								- dt/dy*(p[i][j+1]-p[i][j]) 
-									+ dt*1.0/Re*( (v[i+1][j]-2.0*v[i][j]+v[i-1][j])/dx/dx+(v[i][j+1]-2.0*v[i][j]+v[i][j-1])/dy/dy );
-			}
-		}
-		
-		// Boundary conditions
-		for (j=1; j<=(grid-2); j++)
-		{
-			vn[0][j] = -vn[1][j];
-			vn[grid][j] = -vn[grid-1][j];
-		}		
-
-		for (i=0; i<=(grid); i++)
-		{
-			vn[i][0] = 0.0;
-			vn[i][grid-1] = 0.0;
-		}		
-	
-		// Solves continuity equation
-		for (i=1; i<=(grid-1); i++)
-		{
-			for (j=1; j<=(grid-1); j++)
-			{
-				pn[i][j] = p[i][j]-dt*delta*(  ( un[i][j]-un[i-1][j] )/dx + ( vn[i][j]-vn[i][j-1] ) /dy  );
-			}
-		}
-		
-		
-		// Boundary conditions
-		for (i=1; i<=(grid-1); i++)
-		{
-			pn[i][0] = pn[i][1];
-			pn[i][grid] = pn[i][grid-1];
-		}
-		
-		for (j=0; j<=(grid); j++)
-		{
-			pn[0][j] = pn[1][j];
-			pn[grid][j] = pn[grid-1][j];
-		}		
-		
-		// Displaying error
-		error = 0.0;
-		
-		for (i=1; i<=(grid-1); i++)
-		{
-			for (j=1; j<=(grid-1); j++)
-			{
-				m[i][j] = (  ( un[i][j]-un[i-1][j] )/dx + ( vn[i][j]-vn[i][j-1] )/dy  );
-				error = error + fabs(m[i][j]);
-			}
-		}
-		
-		if (step%1000 ==1)
-		{
-	    printf("Error is %5.8lf for the step %d\n", error, step);
-		}
-		
-		
-		// Iterating u
-		for (i=0; i<=(grid-1); i++)
-		{
-			for (j=0; j<=(grid); j++)
-			{
-				u[i][j] = un[i][j];
-			}
-		}
-		
-		// Iterating v
-		for (i=0; i<=(grid); i++)
-		{
-			for (j=0; j<=(grid-1); j++)
-			{
-				v[i][j] = vn[i][j];
-			}
-		}
-		
-		// Iterating p
-		for (i=0; i<=(grid); i++)
-		{
-			for (j=0; j<=(grid); j++)
-			{
-				p[i][j] = pn[i][j];
-			}
-		}
-
-		step = step + 1;
-	
-	}
-	
-	for (i=0; i<=(grid-1); i++)
-	{
-		for (j=0; j<=(grid-1); j++)
-		{	
-			uc[i][j] = 0.5*(u[i][j]+u[i][j+1]);
-			vc[i][j] = 0.5*(v[i][j]+v[i+1][j]);
-			pc[i][j] = 0.25*(p[i][j]+p[i+1][j]+p[i][j+1]+p[i+1][j+1]);
-		}
-	}
-	
-	
-	
-	// OUTPUT DATA
-	FILE *fout2, *fout3;
-	fout2 = fopen("UVP.txt","w+t");
-	fout3 = fopen("Central_U.txt","w+t");
-
-	if ( fout2 == NULL )
-	{
-    printf("\nERROR when opening file\n");
-    fclose( fout2 );
-	}
-
-  else
-	{
-	fprintf( fout2, "VARIABLES=\"X\",\"Y\",\"U\",\"V\",\"P\"\n");
-	fprintf( fout2, "ZONE  F=POINT\n");
-	fprintf( fout2, "I=%d, J=%d\n", grid, grid );
-
-	for ( j = 0 ; j < (grid) ; j++ )
-	{
-    for ( i = 0 ; i < (grid) ; i++ )
-    {
-		double xpos, ypos;
-		xpos = i*dx;
-		ypos = j*dy;
-
-		fprintf( fout2, "%5.8lf\t%5.8lf\t%5.8lf\t%5.8lf\t%5.8lf\n", xpos, ypos, uc[i][j], vc[i][j], pc[i][j] );
-    }
-	}
-	}
-
-	fclose( fout2 );
-	
-	// CENTRAL --U
-  fprintf(fout3, "VARIABLES=\"U\",\"Y\"\n");
-  fprintf(fout3, "ZONE F=POINT\n");
-  fprintf(fout3, "I=%d\n", grid );
-
-  for ( j = 0 ; j < grid ; j++ )
-  {
-	double ypos;
-    ypos = (double) j*dy;
-
-    fprintf( fout3, "%5.8lf\t%5.8lf\n", (uc[grid/2][j] + uc[(grid/2)+1][j])/(2.), ypos );
-  }
-
+#include<stdio.h>
+#include<math.h>
+int main(){
+double L=1,H=1,delta_x,delta_y,beta,Re=400; // parameters
+int imax=129,jmax=129; // imax=jmax=129 for 129 x 129 grid
+delta_x=L/(imax-1);delta_y=H/(jmax-1);
+beta=delta_x/delta_y;
+double
+si_new[imax+1][jmax+1],omega_new[imax+1][jmax+1],si_old[imax+1][jmax+1],omega_ol
+d[imax+1][jmax+1],u[imax+1][jmax+1],v[imax+1][jmax+1]; // variable to store data
+int i,j,k=0;
+for(j=1;i<=jmax;j++){
+for(i=1;i<=imax;i++){
+si_old[i][j]=0;
+si_new[i][j]=0;
+omega_old[i][j]=0;
+omega_new[i][j]=0;
+u[i][j]=0;
+v[i][j]=0;
+// assign all variable (si,omega,u,v ) equal to zero, so random value not
+assign to them
+}
+}
+for(i=2;i<=imax-1;i++){
+u[i][jmax]=1; // u value asssign at top to zero
+}
+for(j=1;j<=jmax;j++){
+for(i=1;i<=imax;i++){
+if(j==1){
+omega_new[i][j]=-2*(si_new[i][j+1])/(pow(delta_y,2));
+// boundary condition at y=0 (bottom wall)
+}
+else if(j==jmax){
+omega_new[i][j]=-2*(si_new[i][j-1]+u[i][j])/(pow(delta_y,2));
+// boundary condition at y=H=1 (top wall)
+}
+else if(i==1){
+omega_new[i][j]=-2*(si_new[i+1][j])/(pow(delta_x,2));
+// boundary condition at x=0 (left wall)
+}
+else if(i==imax){
+omega_new[i][j]=-2*(si_new[i-1][j])/(pow(delta_x,2));
+// boundary condition at x=L=1 (right wall)
+}
+else{
+omega_new[i][j]=0; // inner points
+}
+}
+} // boundary condition initialization
+int itr=0; // initialize iteration count as 0
+double error=pow(10,1), sum_omega=0; // any large error is assumed, ans
+sum omega to store sum of all omega
+while(error > pow(10,-5)){ // stoping condition ( when error < 10^-5 it
+stops)
+for(j=1;j<=jmax;j++){
+for(i=1;i<=imax;i++){
+si_old[i][j]=si_new[i][j]; // to assign new value to old
+value so new calculation can be done
+omega_old[i][j]=omega_new[i][j];
+}
+}
+for(k=1;k<=10;k++){ // inner iteration of si ( 10 is
+used)
+for(j=2;j<=jmax-1;j++){
+for(i=2;i<=imax-1;i++){
+si_new[i][j]=(si_new[i+1][j]+si_new[i1][j]+pow(beta,2)*(si_new[i][j+1]+si_new[i][j-1])+ pow(delta_x,2)*omega_new[i][j]) /
+(2.0*(1.0+pow(beta,2)));
+// update si
+}
+}
+}
+for(j=1;j<=jmax;j++){
+for(i=1;i<=imax;i++){
+if(j==1){
+omega_new[i][j]=-2*(si_new[i][j+1])/(pow(delta_y,2));
+// boundary condition at y=0 (bottom wall)
+}
+else if(j==jmax){
+omega_new[i][j]=-2*(si_new[i][j1]+u[i][j])/(pow(delta_y,2)); // boundary condition at y=H=1 (top wall)
+}
+else if(i==1){
+omega_new[i][j]=-2*(si_new[i+1][j])/(pow(delta_x,2));
+// boundary condition at x=0 (left wall)
+}
+else if(i==imax){
+omega_new[i][j]=-2*(si_new[i-1][j])/(pow(delta_x,2));
+// boundary condition at x=L=1 (right wall)
+}
+}
+} // boundary condition at each iteration
+for(k=1;k<=2;k++){ // inner iteration of omega ( 2 is used)
+for(j=2;j<=jmax-1;j++){
+for(i=2;i<=imax-1;i++){
+omega_new[i][j]=(omega_new[i+1][j] +
+omega_new[i-1][j] + pow(beta,2)*(omega_new[i][j+1] + omega_new[i][j-1]) -
+0.25*beta*Re*(omega_new[i+1][j] - omega_new[i-1][j])*(si_new[i][j+1] - si_new[i][j-1]) +
+0.25*beta*Re*(omega_new[i][j+1] - omega_new[i][j-1])*(si_new[i+1][j] - si_new[i-1][j])) /
+(2.0*(1.0+pow(beta,2)));
+// update omega
+}
+}
+}
+error=0;
+sum_omega=0;
+for(i=2;i<=jmax-1;i++){
+for(j=2;j<=imax-1;j++){
+error=error+fabs(omega_new[i][j]-omega_old[i][j]);
+// summation | omega^(k+1) - omega^(k) | calculated
+sum_omega=sum_omega+fabs(omega_new[i][j]);
+// summation of | omega^(k+1) | calculated
+}
+}
+error=error/sum_omega; // | omega^(k+1) - omega^(k) | / |
+omega^(k+1) |
+itr=itr+1; // increament iteration count
+}
+for(j=2;j<=jmax-1;j++){
+for(i=2;i<=imax-1;i++){
+u[i][j]=(si_new[i][j+1] - si_new[i][j-1])/(2.0*delta_y);
+v[i][j]=(si_new[i+1][j] - si_new[i-1][j])/(2.0*delta_x);
+}
+}
+FILE *OUT=fopen("lid-driven_square_cavity_data.txt","w"); // for file storage
+printf("\nu-velocity along the vertical centre line\n");
+fprintf(OUT,"\nu-velocity along the vertical centre line\n");
+for(j=1;j<=jmax;j++){
+printf("%lf\t",u[65][j]);
+fprintf(OUT,"%lf\t",u[65][j]); // u-velocity along the vertical
+centre line storing to file
+}
+printf("\nv-velocity along the horizontal centre line\n");
+fprintf(OUT,"\nv-velocity along the horizontal centre line\n");
+for(i=1;i<=imax;i++){
+printf("%lf\t",v[i][65]);
+fprintf(OUT,"%lf\t",v[i][65]); // v-velocity along the horizontal
+centre line storing to file
+}
+printf("\nsi value\n");
+fprintf(OUT,"\nsi value\n");
+for(j=1;j<=jmax;j++){
+for(i=1;i<=imax;i++){
+printf("%lf\t",si_new[i][j]);
+fprintf(OUT,"%lf\t",si_new[i][j]); // si value storing to file
+}
+printf("\n");
+fprintf(OUT,"\n");
+}
+printf("\nomega value\n");
+fprintf(OUT,"\nomega value\n");
+for(j=1;j<=jmax;j++){
+for(i=1;i<=imax;i++){
+printf("%lf\t",omega_new[i][j]);
+fprintf(OUT,"%lf\t",omega_new[i][j]); // omega value storing to
+file
+}
+printf("\n");
+fprintf(OUT,"\n");
+}
+return 0;
 }
